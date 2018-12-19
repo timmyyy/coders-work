442568e52af9a48bb6f640606a25ee0eae0e9fbc
timmyy <ghost8bit@gmail.com>
2018-12-19 22:38:38 +0300

    init from spring initializr

---
+++ src/main/java/pro/nasibullin/spring/coderswork/CodersWorkApplication.java
@@ -0,0 +1,14 @@
+package pro.nasibullin.spring.coderswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodersWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodersWorkApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/pro/nasibullin/spring/coderswork/CodersWorkApplicationTests.java
@@ -0,0 +1,17 @@
+package pro.nasibullin.spring.coderswork;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodersWorkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
